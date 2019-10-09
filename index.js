5c1f21933fac8977c62ffcb7fd053a5575b91934
Joanne <joannemarie.rocafort@gmail.com>
2019-10-08 21:58:12 -0700

    iteration 1

---
+++ index.js
@@ -0,0 +1,5 @@
+import './node_modules/jquery/dist/jquery.slim.min.js';
+import './node_modules/popper.js/dist/popper.min.js';
+import './node_modules/bootstrap/dist/js/bootstrap.js';
+
+console.log($('h1').text())
