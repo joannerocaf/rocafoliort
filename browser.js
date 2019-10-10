ea8030b0c0e16361e78dae0c1ff7a5fdec7751f1
Zack Schuster <email@zackschuster.com>
2019-10-10 09:12:51 -0700

    style: use flexbox & add active link border

--- browser.js
+++ browser.js
@@ -2,4 +2,7 @@ import './node_modules/jquery/dist/jquery.slim.min.js';
 import './node_modules/popper.js/dist/popper.min.js';
 import './node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
-console.log($('h1').text())
+const page = window.location.pathname.replace('/pages/', '').replace('.html', '');
+const link = document.getElementById(page);
+if (link != null)
+	link.classList.add('border-info');
