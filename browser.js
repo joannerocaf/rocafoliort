cfded11b65ed528e1c58f7910b58f985926723b5
Zack Schuster <email@zackschuster.com>
2019-10-10 14:41:02 -0700

    navbar: add fancy hamburger

--- browser.js
+++ browser.js
@@ -6,3 +6,9 @@ const page = window.location.pathname.replace('/pages/', '').replace('.html', ''
 const link = document.getElementById(page);
 if (link != null)
 	link.classList.add('border-info');
+
+const button = document.querySelector('button.navbar-toggler');
+if (button != null)
+	button.onclick = () => {
+		button.classList.toggle('menu-open')
+	};
