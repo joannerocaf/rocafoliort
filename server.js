8e188558d297de4cb1c4499cf6c43166042ce8c1
Zack Schuster <email@zackschuster.com>
2020-08-28 01:24:42 -0700

    chore: enable response compression

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const compression = require('compression');
 
 express()
+	.use(compression())
 	.use(express.static(__dirname, { index: 'index.html', extensions: ['html'] }))
 	.use(express.static('pages', { extensions: ['html'] }))
 	.listen(Number(process.env.PORT || '8080'));
